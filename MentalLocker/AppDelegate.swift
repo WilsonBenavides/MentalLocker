3de248ee90d925cd48329711ccc0b36a8e5c69ce
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-06-29 01:03:57 -0700

    red cells device width

--- MentalLocker/AppDelegate.swift
+++ MentalLocker/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print(UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        let layout = UICollectionViewFlowLayout()
+        window?.rootViewController = UINavigationController(rootViewController: HomeController(collectionViewLayout: layout))
         
         return true
     }
