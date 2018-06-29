2ab9a4eb877be37ac65d78430691c8ecc8d5c834
Wilson Benavides <benavidesb@unicauca.edu.co>
2018-06-28 20:12:04 -0700

    red background programatically, first step

--- MentalLocker/ViewController.swift
+++ MentalLocker/ViewController.swift
@@ -5,21 +5,12 @@
 //  Created by willix on 25/06/18.
 //  Copyright © 2018 willix. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
